465ec8c32a5c1a18d8b7e7c9785cc55a345faa54
Jerome <jeromegarcia.16@gmail.com>
2024-06-18 22:08:51 +0800

    Created Kafka Producer

---
+++ src/main/java/com/codelab/springboot/kafka/KafkaProducer.java
@@ -0,0 +1,21 @@
+package com.codelab.springboot.kafka;
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class KafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message){
+        LOGGER.info(String.format("Message sent %s", message));
+        kafkaTemplate.send("Javaguides", message);
+    }
+}
